5b8f8ca27e7fa0ecf42a746499c0e234930907da
RedMalachite <anluvi@ukr.net>
2023-09-03 01:36:23 +0300

    020923

---
+++ app/db.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+
+function dbSelect(Tables $table, string $columns = '*', string $condition = null, string $order = null, bool $isSingle = false): array
+{
+    $sql = "SELECT {$columns} FROM {$table -> value}";
+    $sql .= $condition ? " WHERE {$condition}" : "";
+    $sql .= $order ? " ORDER BY {$order}" : "";
+
+
+    $query = DB::connect()->prepare($sql);
+    $query -> execute();
+
+    $result = $isSingle ? $query->fetch() : $query->fetchAll();
+
+    return $result ?? [];
+};
+dd(
+    dbSelect(Tables::Content, condition: 'id = 3', isSingle: false)
+);
--- app/helpers.php
+++ app/helpers.php
@@ -4,3 +4,4 @@ function getUrl(): string
     $uri = trim($_SERVER['REQUEST_URI'], '/');
     return explode('?', $uri)[0];
 };
+
