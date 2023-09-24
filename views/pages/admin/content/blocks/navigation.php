36b9e465015c871ed6fff29efd212333895f01c5
RedMalachite <anluvi@ukr.net>
2023-09-24 18:59:02 +0300

    Works

--- views/pages/admin/content/blocks/navigation.php
+++ views/pages/admin/content/blocks/navigation.php
@@ -8,72 +8,65 @@ $fields = json_decode($block['content'], true);
             <div class="col-2"></div>
             <div class="col-8">
                 <div class="card">
-                    <div class="card-header text-center">
-                        <h3>Block: <?= $block['name'] ?></h3>
-                        <form action="/" method="POST" enctype="multipart/form-data">
-                            <input type="hidden" name="type" value="edit_content">
-                            <input type="hidden" name="block_id" value="<?= $block['id'] ?>">
-                            <input type="hidden" name="block_name" value="<?= $block['name'] ?>">
-                            <div class="cart-body">
-                                <?php
-                                if (!empty($fields['logo'])):
-                                    ?>
-                                    <div class="mt-3">
-                                        <p>Current logo:</p>
-                                        <img class="img-thumbnail" src="/img/<?= $fields['logo'] ?>" alt="">
-                                    </div>
-                                <?php endif; ?>
-                                <div class="mt-3">
-                                    <label for="logo">Upload new logo</label>
-                                    <input type="file" class="form-control" name="Logo">
-                                </div>
-                                <div class="mt-3">
-                                    <h5>Nav links:</h5>
-                                    <?php $lastKey = array_key_last($fields['links']) ?? 0;
-                                    ?>
-                                    <?php if (!empty($fields['links'])): ?>
-                                        <?php
-                                        foreach ($fields['links'] as $key => $link):
-                                            ?>
-                                            <div class="row d-flex align-items-center" style="border: 1px solid #000;border-radius: 15px">
-                                                <div class="col-11 mb-3">
-                                                    <label for="link_text_<?= $key ?>">Title</label>
-                                                    <input type="text"
-                                                           class="form-control"
-                                                           id="link-text_<?= $key ?>"
-                                                           value="<?= $link['title'] ?>"
-                                                           name="links[<?= $key ?>][title]"
-                                                    >
-                                                    <label for="link_<?= $key ?>">Link</label>
-                                                    <input type="text"
-                                                           class="form-control"
-                                                           id="link_<?= $key ?>"
-                                                           value="<?= $link['href'] ?>"
-                                                           name="links[<?= $key ?>][href]"
-                                                    >
-                                                </div>
-                                                <div class="col-1 d-flex align-items-center">
-                                                    <a href="#" class="btn btn-outline-danger"><i
-                                                                class="fa fa-trash"></i></a>
-                                                </div>
-                                            </div>
-                                        <br>
-                                        <?php
-                                        endforeach;
-                                        ?>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                            <div class="cart-footer">
+                    <div class="card-header text-center"><h3>Block: "<?= $block['name'] ?>"</h3></div>
+                    <form action="/" method="POST" enctype="multipart/form-data">
+                        <input type="hidden" name="type" value="edit_content">
+                        <input type="hidden" name="block_id" value="<?= $block['id'] ?>">
+                        <input type="hidden" name="block_name" value="<?= $block['name'] ?>">
 
+                        <div class="card-body">
+                            <?php if (!empty($fields['logo'])): ?>
+                                <div class="mb-3">
+                                    <p>Current logo: </p>
+                                    <img class="img-thumbnail w-50" src="<?= IMAGES_URI . "/$fields[logo]" ?>"/>
+                                </div>
+                            <?php endif; ?>
+
+                            <div class="mb-3">
+                                <label for="logo">Upload new logo</label>
+                                <input type="file" class="form-control" name="logo"/>
                             </div>
-                            <button type="submit" class="btn btn-outline-primary mt-3">Update info</button>
-                        </form>
-                        <br>
-                        <a href="/admin/content">
-                            <button type="submit" class="btn btn-outline-primary">Back</button>
-                        </a>
-                    </div>
+
+                            <div class="mb-3">
+                                <h5>Links: </h5>
+                            </div>
+
+                            <?php $lastKey = array_key_last($fields['links']) ?? 0; ?>
+                            <div class="mb-3 col-6 offset-3 links-wrapper" data-last_key="<?= $lastKey ?>">
+                                <?php if (!empty($fields['links'])): ?>
+                                    <?php foreach ($fields['links'] as $key => $link): ?>
+                                        <div class="row mb-3 d-flex align-items-center justify-content-center bg-body-secondary p-2 links-item"
+                                        >
+                                            <div class="col-9">
+                                                <label for="link_text_<?= $key ?>">Title</label>
+                                                <input type="text"
+                                                       class="form-control"
+                                                       id="link_text_<?= $key ?>"
+                                                       value="<?= $link['title'] ?>"
+                                                       name="links[<?= $key ?>][title]"
+                                                >
+                                                <label for="link_<?= $key ?>">Link</label>
+                                                <input type="text"
+                                                       class="form-control"
+                                                       id="link_<?= $key ?>"
+                                                       value="<?= $link['href'] ?>"
+                                                       name="links[<?= $key ?>][href]"
+                                                >
+                                            </div>
+                                            <div class="col-3 text-center">
+                                                <a href="#" class="btn btn-outline-danger remove-link"><i class="fa fa-trash"></i></a>
+                                            </div>
+                                        </div>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                            </div>
+                            <a href="#" class="btn btn-outline-info add-new-link">Add link</a>
+                        </div>
+
+                        <div class="card-footer d-flex align-items-center justify-content-end">
+                            <button type="submit" class="btn btn-outline-primary">Update</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- views/pages/admin/parts/footer.php
+++ views/pages/admin/parts/footer.php
@@ -1,4 +1,7 @@
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script type="application/javascript" src="/assets/js/script.js"></script>
+<script type="application/javascript" src="/assets/js/admin.js"></script>
 <script src="<?php BASE_DIR ?>/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
